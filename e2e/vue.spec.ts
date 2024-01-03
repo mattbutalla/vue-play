aa350b45286ccb7390f4ad918f099653c79de351
Matt Butalla <butallmj@gmail.com>
2024-01-02 20:49:16 -0600

    example api mock

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -6,3 +6,55 @@ test('visits the app root url', async ({ page }) => {
   await page.goto('/');
   await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
 })
+
+test("mocks a character and doesn't call api", async ({ page }) => {
+  // Mock the api call before navigating
+  await page.route('https://swapi.dev/api/people/?search=sky', async route => {
+    const json = {
+      "count": 1,
+      "next": null,
+      "previous": null,
+      "results": [
+        {
+          "name": "Chewbacca",
+          "height": "228",
+          "mass": "112",
+          "hair_color": "brown",
+          "skin_color": "unknown",
+          "eye_color": "blue",
+          "birth_year": "200BBY",
+          "gender": "male",
+          "homeworld": "https://swapi.dev/api/planets/14/",
+          "films": [
+            "https://swapi.dev/api/films/1/",
+            "https://swapi.dev/api/films/2/",
+            "https://swapi.dev/api/films/3/",
+            "https://swapi.dev/api/films/6/"
+          ],
+          "species": [
+            "https://swapi.dev/api/species/3/"
+          ],
+          "vehicles": [
+            "https://swapi.dev/api/vehicles/19/"
+          ],
+          "starships": [
+            "https://swapi.dev/api/starships/10/",
+            "https://swapi.dev/api/starships/22/"
+          ],
+          "created": "2014-12-10T16:42:45.066000Z",
+          "edited": "2014-12-20T21:17:50.332000Z",
+          "url": "https://swapi.dev/api/people/13/"
+        }
+      ]
+    };
+    await route.fulfill({ json });
+  });
+
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('link', { name: 'About' }).click();
+  await page.locator('#searchText').click();
+  await page.locator('#searchText').fill('sky');
+  await page.getByRole('button', { name: 'Search' }).click();
+
+  await expect(page.locator('ul')).toContainText('Chewbacca');
+})
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,43 @@
+<script lang="ts" setup>
+
+import axios from "axios"
+import { ref } from "vue"
+
+const searchText = ref("")
+interface APIResults {
+  results?: any;
+}
+const results = ref<APIResults>({})
+const fetchData = async ()=> {
+  const response = await axios.get(`https://swapi.dev/api/people/?search=${searchText.value}`);
+  results.value = response.data;
+}
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <label for="searchInput">Search:</label>
+    <input v-model="searchText" id="searchText" />
+    <button @click="fetchData">Search</button>
+    <ul>
+      <li v-for="result in results.results" :key="result.url">
+        <dl>
+          <dt>name</dt>
+          <dd>{{ result.name }}</dd>
+        </dl>
+        <dl>
+          <dt>height</dt>
+          <dd>{{ result.height }}</dd>
+        </dl>
+        <dl>
+          <dt>mass</dt>
+          <dd>{{ result.mass }}</dd>
+        </dl>
+      </li>
+    </ul>
+    <pre>
+      {{ results }}
+    </pre>
   </div>
 </template>
 
